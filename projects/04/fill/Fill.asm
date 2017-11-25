461c317e09051969f90698e65ca2266dc59bd79a
Arlandis Word <arlandislawrence@gmail.com>
2017-11-25 14:46:34 -0500

    Finished the Fill assembly program

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -6,9 +6,76 @@
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
+// the screen should remain fully black as long as the key is pressed.
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+@8192
+D=A
+@num_cells
+M=D
+(LISTEN_FOR_INPUT)
+  // grab from the keyboard memory mapped region to see if
+  // anything is being pressed
+  @KBD
+  D=M
+
+  // if register == 0, i.e. if user is not pressing anything
+  @clear_counter
+  M=0
+  @NO_USER_INPUT
+  D;JEQ
+
+  @draw_counter
+  M=0
+  @DRAW_SCREEN
+  0;JMP
+
+(DRAW_SCREEN)
+  @clear_counter
+  M=0
+  // see if we've drawn all the pixels
+  @num_cells
+  D=M
+  @draw_counter
+  D=D-M
+  @LISTEN_FOR_INPUT
+  D;JLE
+
+  // get the screen + offset address and write all the pixels to it
+  @draw_counter
+  D=M
+  @SCREEN
+  A=D+A
+  M=-1
+
+  @draw_counter
+  M=M+1
+
+  @DRAW_SCREEN
+  0;JMP
+
+(NO_USER_INPUT)
+  @draw_counter
+  M=0
+  // see if we've drawn all the pixels
+  @num_cells
+  D=M
+  @clear_counter
+  D=D-M
+  @LISTEN_FOR_INPUT
+  D;JLE
+
+  // get the screen + offset address and write all the pixels to it
+  @clear_counter
+  D=M
+  @SCREEN
+  A=D+A
+  M=0
+
+  @clear_counter
+  M=M+1
+  @NO_USER_INPUT
+  0;JMP
+
